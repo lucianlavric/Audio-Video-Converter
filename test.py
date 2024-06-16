93da187dd236f53d1eb4e409182fd2d2cbfaacb3
iamlucianlavric <lucianlavric2005@gmail.com>
2024-06-16 15:37:57 -0400

    Added downloading option to local machine

---
+++ test.py
@@ -0,0 +1,21 @@
+from pytube import YouTube
+import os
+
+
+## Documentation at https://code.pieces.app/blog/how-to-download-a-youtube-video-in-mp3-format-with-python
+
+
+video = YouTube(str(input("Enter the URL of the video you want to download: \n")))
+
+audio = video.streams.filter(only_audio = True).first()
+
+
+print("Enter the destination (leave blank for current directorty)")
+destination = str(input(">>")) or '.'
+
+out_file = audio.download(output_path=destination)
+base, ext = os.path.splitext(out_file)
+new_file = base + '.mp3'
+os.rename(out_file, new_file)
+
+print(video.title + "has been successfully downloaded in .mp3 format")
